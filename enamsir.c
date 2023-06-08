274486d1d286b60edddf089af9bf3a33febede81
prit0m <59039346+pritomshad@users.noreply.github.com>
2023-06-08 05:15:18 +0000

    t

--- enamsir.c
+++ enamsir.c
@@ -8,17 +8,22 @@
 #define pi acos(-1.0)
 
 /**********************functions***********************/
-
-
-
-
-
-
-
+int a[101][100];
+int track[101][101];
 /*********************functions***********************/
 int main()
 {
-
+    int t,n,m;
+    scanf("%d%d%d",&t,&n,&m);
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
+            scanf("%d",&a[i][j]);
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
+        {
+            
+        }
+    
 
     return 0;
 }
