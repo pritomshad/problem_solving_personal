5efc04e34d3923fe07cefa67938fa92d529ac214
prit0m <59039346+pritomshad@users.noreply.github.com>
2023-06-08 04:25:40 +0000

    hell yeah

---
+++ enamsir.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdint.h>
+#include<stdbool.h>
+#define pi acos(-1.0)
+
+/**********************functions***********************/
+
+
+
+
+
+
+
+/*********************functions***********************/
+int main()
+{
+
+
+    return 0;
+}
